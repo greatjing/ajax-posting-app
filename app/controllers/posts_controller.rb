fee651068d5264b9f40f9829badf5d38ef601077
greatjing <greatjing@sina.cn>
2017-07-21 13:47:49 +0800

    add like function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,23 @@ class PostsController < ApplicationController
     # render :js => "alert('ok');"
   end
 
+  def like
+    @post = Post.find(params[:id])
+    unless @post.find_like(current_user)
+      Like.create( :user => current_user, :post => @post )
+    end
+
+    redirect_to posts_path
+  end
+
+  def unlike
+    @post = Post.find(params[:id])
+    like = @post.find_like(current_user)
+    like.destroy
+
+    redirect_to posts_path
+  end
+
   protected
 
   def post_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
   has_many :posts
+
+  has_many :likes, :dependent => :destroy
+  has_many :liked_posts, :through => :likes, :source => :post
+
   def display_name
     # 取email的前半显示
     self.email.split("@").first
